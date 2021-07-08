09bbbe16aad4ec791655efb6c6b4d3e916be2e70
sonumontenegro@gmail.com <sonumontenegor@gmail.com>
2021-07-08 12:21:22 +0530

    first commit for JS

---
+++ JAVASCRIPT/ConnectionToDB.js
@@ -0,0 +1,19 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "database-1.cyc2ywmoxfol.ap-south-1.rds.amazonaws.com",
+  user: "admin",
+  password: "admin1234",
+  database: "trailDatabase"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  var sql = "INSERT INTO TRAIL VALUES('Abhay Mukul',20)";
+  con.query(sql,function(err,result){
+      if (err) throw err;
+      console.log(result);
+      console.log("done")
+      })
+});
